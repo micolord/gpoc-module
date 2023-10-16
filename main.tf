e5e2b47093d44d48228f157d3e9197072f944cb0
mico <mico.viola@myecloud.net>
2023-10-16 13:58:18 +0800

    standardized

--- main.tf
+++ main.tf
@@ -5,3 +5,35 @@ provider "aws" {
 terraform {
     required_version = ">=1.0"
 }
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "${var.env_name}-${var.project}-GO1"
+
+  instance_type          = var.ec2_instance_type
+  ami                    = var.ec2_ami_id
+  key_name               = "user1"
+  monitoring             = true
+  user_data              = file("userdata.sh")
+  associate_public_ip_address = true
+  iam_instance_profile   = "MySessionManagerRole"
+  vpc_security_group_ids = [module.GO_sg.security_group_id]
+  subnet_id              = var.public_subnet_id_1
+
+  tags = {
+    Environment = var.env_name
+    Project = var.project
+
+  }
+}
+
+module "GO_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
+
+  name        = "${var.env_name}-${var.project}-GO-sg"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = "vpc-015d0d851ac305f9d"
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+}
