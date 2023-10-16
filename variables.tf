609181a2146d92c554f49396ff2d028cfaaa90b6
mico <mico.viola@myecloud.net>
2023-10-16 14:35:24 +0800

    standardized

--- variables.tf
+++ variables.tf
@@ -1,79 +1,24 @@
-variable "region" {
-    description = "AWS deployment region"
-}
-
-variable "project" {
-    description = "Project name"
-}
-
-variable "env_name" {
-    description = "Environment Name"
-}
-
-variable "vpc_cidr" {
-    description = "VPC CIDR block"
-}
-
-variable "vpc_id" {
-    description = "VPC ID"
-}
-
-#variable "public_subnet_cidr_1" {
-#    description = "Public subnet CIDR block"
-#}
-
-#variable "public_subnet_cidr_2" {
-#    description = "Public subnet CIDR block"
-#}
-
-variable "public_subnet_id_1" {
-    description = "Public subnet CIDR block"
-}
-
-variable "public_subnet_id_2" {
-    description = "Public subnet CIDR block"
-}
-
-#variable "public_subnet_cidr_3" {
-#    description = "Public subnet CIDR block"
-#}
-
-#variable "private_subnet_cidr_1" {
-#    description = "Private subnet CIDR block"
-#}
-
-#variable "private_subnet_cidr_2" {
-#    description = "Private subnet CIDR block"
-#}
-
-variable "private_subnet_id_1" {
-    description = "Private subnet CIDR block"
-}
-
-variable "private_subnet_id_2" {
-    description = "Private subnet CIDR block"
-}
-
-#variable "private_subnet_cidr_3" {
-#    description = "Private subnet CIDR block"
-#}
-
-variable "availability_zone_1" {
-    description = "Availability Zone A"
-}
-
-variable "availability_zone_2" {
-    description = "Availability Zone B"
+variable "ec2_instance_type" {
+  description = "The EC2 instance type"
+  type        = string
 }
 
 variable "ec2_ami_id" {
-    description = "Instance AMI"
+  description = "The ID of the Amazon Machine Image (AMI)"
+  type        = string
 }
 
-variable "ec2_instance_type" {
-    description = "Instance type"
+variable "env_name" {
+  description = "Environment name"
+  type        = string
 }
 
-#variable "master_instance_class" {
- #   description = "Instance type"
-#}
+variable "project" {
+  description = "Project name"
+  type        = string
+}
+
+variable "public_subnet_id_1" {
+  description = "ID of the public subnet"
+  type        = string
+}
